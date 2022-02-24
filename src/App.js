83797a79a2dde1a337f82f5fca4517b899d1671b
madstone-dev <hanbin8570@gmail.com>
2022-02-24 13:57:44 +0900

    Style : 드롭다운 예제 수정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ChevronDoubleLeft from "./components/icons/ChevronDoubleLeft";
 import ChevronDoubleRight from "./components/icons/ChevronDoubleRight";
 import Cog from "./components/icons/Cog";
 import Refresh from "./components/icons/Refresh";
+import Input from "./components/Input";
 import ListItem from "./components/ListItem";
 import Selector from "./components/Selector";
 import StackedList from "./components/StackedList";
@@ -35,9 +36,55 @@ function App() {
           >
             <StackedList>
               <ListItem>
-                <div className="p-3 flex justify-between">
+                <div className="p-3 flex justify-between items-center">
                   <span className="text-lg">타이틀</span>
                   <span>토글</span>
+                </div>
+              </ListItem>
+              <ListItem>
+                <div className="p-3">
+                  <div>
+                    <Input placeholder={"avaliable options"} />
+                  </div>
+                  <div>
+                    <Input placeholder={"selected options"} />
+                  </div>
+                </div>
+              </ListItem>
+              <ListItem>
+                <div className="p-3 flex justify-between items-center">
+                  <span className="text-lg">검색</span>
+                  <span>토글</span>
+                </div>
+              </ListItem>
+              <ListItem>
+                <div className="p-3 flex justify-between items-center">
+                  <span className="text-lg">하나씩만 옮기기</span>
+                  <span>토글</span>
+                </div>
+              </ListItem>
+              <ListItem>
+                <div className="p-3 flex justify-between items-center">
+                  <span className="text-lg">선택된 아이템 갯수 표시</span>
+                  <span>토글</span>
+                </div>
+              </ListItem>
+              <ListItem>
+                <div className="p-3 flex items-center space-x-2">
+                  <span className="text-lg">아이템 크기</span>
+                  <span>xs</span>
+                  <span>s</span>
+                  <span>m</span>
+                </div>
+              </ListItem>
+              <ListItem>
+                <div className="p-3">
+                  <div>
+                    <Input placeholder={`가로 (현재: 171px)`} />
+                  </div>
+                  <div>
+                    <Input placeholder={`세로 (현재: 300px)`} />
+                  </div>
                 </div>
               </ListItem>
             </StackedList>

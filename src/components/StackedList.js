b9442adeab0ea80c9b1fccc32333650758288c42
madstone-dev <hanbin8570@gmail.com>
2022-02-24 17:12:59 +0900

    Fix : ...rest 추가 했습니다.

--- src/components/StackedList.js
+++ src/components/StackedList.js
@@ -1,6 +1,10 @@
-export default function StackedList({ children }) {
+export default function StackedList({
+  className = 'bg-white shadow overflow-y-auto sm:rounded-md flex-1',
+  children,
+  ...rest
+}) {
   return (
-    <div className="bg-white shadow overflow-y-auto sm:rounded-md flex-1">
+    <div className={className} {...rest}>
       <div role="list" className="divide-y divide-gray-200">
         {children}
       </div>

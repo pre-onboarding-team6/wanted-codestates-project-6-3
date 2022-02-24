6c57c0c8dc3b8a5ccdf3a989eb5954422b8a76de
madstone-dev <hanbin8570@gmail.com>
2022-02-24 16:11:56 +0900

    Fix : ...rest 추가

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,8 @@
-export default function Input({ type = "text", placeholder }) {
+export default function Input({ type = 'text', placeholder, ...rest }) {
   return (
     <div>
       <input
+        {...rest}
         type={type}
         className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
         placeholder={placeholder}

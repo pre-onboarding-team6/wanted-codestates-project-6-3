2b00f4631a6542d5973ca8725d712728d7e2c20f
madstone-dev <hanbin8570@gmail.com>
2022-02-24 13:52:47 +0900

    Style : Dropdown 예제입니다.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import Button from "./components/Button";
+import Dropdown from "./components/Dropdown";
 import ChevronDoubleLeft from "./components/icons/ChevronDoubleLeft";
 import ChevronDoubleRight from "./components/icons/ChevronDoubleRight";
+import Cog from "./components/icons/Cog";
 import Refresh from "./components/icons/Refresh";
+import ListItem from "./components/ListItem";
 import Selector from "./components/Selector";
+import StackedList from "./components/StackedList";
 
 function App() {
   return (
-    <div className="p-6 flex items-center space-x-3">
-      <Selector />
-      <div className="flex flex-col">
-        <Button>
-          <Refresh />
-        </Button>
-        <Button>
-          <ChevronDoubleLeft />
-        </Button>
-        <Button>
-          <ChevronDoubleRight />
-        </Button>
+    <div>
+      <div className="p-6 flex items-center space-x-3">
+        <Selector />
+        <div className="flex flex-col">
+          <Button>
+            <Refresh />
+          </Button>
+          <Button>
+            <ChevronDoubleLeft />
+          </Button>
+          <Button>
+            <ChevronDoubleRight />
+          </Button>
+        </div>
+        <Selector />
+        <div className="p-6">
+          <Dropdown
+            button={
+              <Button>
+                <Cog />
+              </Button>
+            }
+          >
+            <StackedList>
+              <ListItem>
+                <div className="p-3 flex justify-between">
+                  <span className="text-lg">타이틀</span>
+                  <span>토글</span>
+                </div>
+              </ListItem>
+            </StackedList>
+          </Dropdown>
+        </div>
       </div>
-      <Selector />
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,14 +1,14 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
-import { Transition } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 
 export default function Dropdown({ button, children }) {
   return (
-    <div className="relative inline-block text-left">
+    <Menu as="div" className="relative inline-block text-left">
       <div>
-        <button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-          {button}
-        </button>
+        <Menu.Button as="div">
+          <div>{button}</div>
+        </Menu.Button>
       </div>
 
       <Transition
@@ -20,10 +20,10 @@ export default function Dropdown({ button, children }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           {children}
-        </div>
+        </Menu.Items>
       </Transition>
-    </div>
+    </Menu>
   );
 }

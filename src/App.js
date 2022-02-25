f1702b994f3ba1fae164c93b9be0a29f886028fd
madstone-dev <hanbin8570@gmail.com>
2022-02-25 10:53:58 +0900

    Style : 가운데 정렬 및 버튼 간격

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
   };
 
   return (
-    <div className="flex p-6 space-x-3">
+    <div className="flex justify-center w-full p-6 space-x-3">
       <Selector
         list={leftItems}
         selectedItems={selectedItems.left}
@@ -78,7 +78,7 @@ function App() {
         itemSize={itemSize}
         showSelected={showSelected}
       />
-      <div className="flex flex-col self-center">
+      <div className="flex flex-col self-center space-y-1">
         <Button onClick={resetMove}>
           <Refresh />
         </Button>
